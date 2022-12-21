e3ff202baf7ed5c8f260692b46522b55cd030b8b
Savio <saviolexdier@gmail.com>
2022-12-21 04:29:18 -0300

    feat: add the card message canvas

--- components/TheCard.js
+++ components/TheCard.js
@@ -1,9 +1,23 @@
-import { Card, Col, Container, Row } from 'react-bootstrap'
+import { Card, Col, Offcanvas, Row } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import { useState } from 'react'
 
 export default function TheCard() {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(!show)
   return (
     <>
-      <Card className="text-center m-4 w-50">
+      <Offcanvas show={show} onHide={handleClose} scroll="scroll">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+      <Card className="m-4 w-50">
         <Card.Header>Featured</Card.Header>
         <Card.Body>
           <Card.Img
@@ -18,11 +32,27 @@ export default function TheCard() {
                 <i class="fa-light fa-share-all"></i>
               </div>
             </Col>
-            <Col/>
+            <Col />
             <Col lg="auto">
               <div>
                 <i class="fa-light fa-bookmark" />
               </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <i class="fa-light fa-person me-2" />
+              <span>Le gusta a 1000 personas</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <span>The Post Comment</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button variant="light" className="p-0" onClick={handleShow}>See Comments</Button>
             </Col>
           </Row>
         </Card.Footer>
