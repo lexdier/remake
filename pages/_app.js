dd199a4d06a670bd703685bd9e9ccd0f53fefd5a
Savio <saviolexdier@gmail.com>
2022-12-25 16:53:45 -0300

    feat: add auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,33 @@
 import '../styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react'
 import Head from 'next/head'
 import TheSidebar from '../components/TheSidebar'
 import { Col, Container, Row } from 'react-bootstrap'
-import TheNavbar from '../components/TheNavbar'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 export default function MyApp({ Component, pageProps }) {
+
   return (
-    <SessionProvider>
+    <Auth0Provider
+      domain="remake.us.auth0.com"
+      clientId="aO59JdSoziMQwish5Sl2Cz3DfHX0suDB"
+      redirectUri={'http://localhost:3000'}>
       <Head>
         <title>
           Remake
         </title>
       </Head>
       <Row className="m-0">
-        <Col lg={4} className="w-25">
+        <Col lg={3}>
           <TheSidebar />
         </Col>
-        <Col lg={8} className="w-75">
-         <Container className="p-3">
-           <Row>
-             <Col>
-               <Row>
-                 <Col>
-                   <TheNavbar />
-                 </Col>
-               </Row>
-               <Row>
-                 <Col className="m-0  p-5 d-flex align-items-center justify-content-center">
-                   <Component {...pageProps} />
-                 </Col>
-               </Row>
-             </Col>
-           </Row>
-         </Container>
+        <Col lg={9}>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
         </Col>
       </Row>
-    </SessionProvider>
+    </Auth0Provider>
 
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,34 @@
-import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import axios from 'axios'
-import { useState } from 'react'
-import { Card, Col, Placeholder } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
 import TheCard from '../components/TheCard'
+import TheNavbar from '../components/TheNavbar'
 
 export default function Home() {
   const router = useRouter()
-  const { data: session, status } = useSession()
-  const [users, setUsers] = useState([]) //terminar
   const posts = [1, 2, 3, 4, 5]
-  useEffect(() => {
-    axios.get('/api/users')
-      // .then(console.log)
-      .then((res) => setUsers(res.data))
-  }, [])
-  if (status === 'loading') {
-    return (
-      <Card className="vh-100">
-        <Card.Body>
-          <Placeholder as={Card.Title} animation="glow">
-            <Placeholder xs={6} />
-          </Placeholder>
-          <Placeholder as={Card.Text} animation="glow">
-            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-            <Placeholder xs={6} /> <Placeholder xs={8} />
-          </Placeholder>
-          <Placeholder.Button variant="primary" xs={6} />
-        </Card.Body>
-      </Card>
-    )
-  }
+
+  // if (status === 'loading') {
+  //  return (
+  //     <Card className="vh-100">
+  //       <Card.Body>
+  //        <Placeholder as={Card.Title} animation="glow">
+  //        <Placeholder xs={6} />
+  //        </Placeholder>
+  //        <Placeholder as={Card.Text} animation="glow">
+  //        <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+  //        <Placeholder xs={6} /> <Placeholder xs={8} />
+  //       </Placeholder>
+  //       <Placeholder.Button variant="primary" xs={6} />
+  //      </Card.Body>
+  //    </Card>
+//    )
+//}
   return (
     <>
-      <div>
+      <TheNavbar />
+      <div className="d-flex align-items-center flex-column">
         {posts.map((post) =>
-          <Col key={post}>
+          <Col key={post} className="w-75 ">
             <TheCard />
           </Col>
         )
