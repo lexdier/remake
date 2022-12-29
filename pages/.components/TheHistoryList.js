726561338df0d750320d5892b81e10adc969a8a7
Savio <saviolexdier@gmail.com>
2022-12-29 04:47:23 -0300

    feat: add middleware

---
+++ pages/.components/TheHistoryList.js
@@ -0,0 +1,31 @@
+import ListGroup from 'react-bootstrap/ListGroup'
+import { Image } from 'react-bootstrap'
+
+export default function TheHistoryList() {
+
+  return (
+    <ListGroup horizontal >
+      <ListGroup.Item className="border-0">
+        <Image roundedCircle src="https://picsum.photos/100/100" alt="pic" />
+      </ListGroup.Item>
+      <ListGroup.Item className="border-0">
+        <Image roundedCircle src="https://picsum.photos/100/100" alt="pic" />
+      </ListGroup.Item>
+      <ListGroup.Item className="border-0">
+        <Image roundedCircle src="https://picsum.photos/100/100" alt="pic" />
+      </ListGroup.Item>
+      <ListGroup.Item className="border-0">
+        <Image roundedCircle src="https://picsum.photos/100/100" alt="pic" />
+      </ListGroup.Item>
+      <ListGroup.Item className="border-0">
+        <Image roundedCircle src="https://picsum.photos/100/100" alt="pic" />
+      </ListGroup.Item>
+      <ListGroup.Item className="border-0">
+        <Image roundedCircle src="https://picsum.photos/100/100" alt="pic" />
+      </ListGroup.Item>
+      <ListGroup.Item className="border-0">
+        <Image roundedCircle src="https://picsum.photos/100/100" alt="pic" />
+      </ListGroup.Item>
+    </ListGroup>
+  )
+}
